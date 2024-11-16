c2fb637fdeda6aa4ce2522f7448c8cc04514b8dd
akashbiswas0 <akash8860biswas@gmail.com>
2024-11-17 04:54:50 +0700

    added openAI package

---
+++ app/creategame/page.js
@@ -0,0 +1,7 @@
+export default function CreateGame(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ app/successful-bet/page.js
@@ -0,0 +1,7 @@
+export default function SuccessfullBet(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
